5a3eb49b58aa67cb28f6df8f6c09fee82c16c65d
fikayosalu <salufikayo@gmail.com>
2025-06-25 22:59:10 +0100

    Set up user Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import AuthController from '../controllers/AuthController';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
